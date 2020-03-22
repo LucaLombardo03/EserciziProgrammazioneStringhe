435d38dbd461dd2fbd46580d639b5e73fcc3990a
luca lombardo <lucalombardo03@gmail.com>
2020-03-22 17:32:11 +0100

    Add project files.

---
+++ EserciziStringhe/MetodiStringhe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace EserciziStringhe
+{
+    public class MetodiStringhe
+    {
+        public static bool AreEquals(string s1, string s2)
+        {
+            if (s1 == s2)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool ContainsDoubleChar(string s)
+        {
+            string a = "A";
+            string e = "E";
+            string i = "I";
+            string o = "O";
+            string u = "U";
+            if (s.Contains(a) || s.Contains(e) || s.Contains(i) || s.Contains(o) || s.Contains(u))
+                return true;
+            else
+                return false;
+        }
+
+        public static string Convert(string s)
+        {
+            string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s);
+            return (titleCase);
+        }
+    }
+}
---
+++ ProvaStringhe/Program.cs
@@ -0,0 +1,27 @@
+﻿using EserciziStringhe;
+using System;
+
+namespace ProvaStringhe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci la stringa:  ");
+            string s1 = Console.ReadLine();
+            Console.Write("Inserisci la stringa:  ");
+            string s2 = Console.ReadLine();
+            bool risposta = MetodiStringhe.AreEquals(s1, s2);
+            Console.WriteLine($"la risposta è {risposta}");
+            Console.Write("Inserisci la stringa:  ");
+            string s3 = Console.ReadLine();
+            bool risposta1 = MetodiStringhe.ContainsDoubleChar(s3);
+            Console.WriteLine($"la risposta è {risposta1}");
+            Console.Write("Inserisci la stringa:  ");
+            string s4 = Console.ReadLine();
+            string risposta2 = MetodiStringhe.Convert(s4);
+            Console.WriteLine(risposta2);
+            Console.ReadLine();
+        }
+    }
+}
