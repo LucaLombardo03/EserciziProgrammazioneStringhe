ae698b948d7c2d7f5246c0a0aa942a7e7d067fae
luca lombardo <lucalombardo03@gmail.com>
2020-03-22 17:33:13 +0100

    add metodi e console app

--- ProvaStringhe/Program.cs
+++ ProvaStringhe/Program.cs
@@ -7,16 +7,21 @@ namespace ProvaStringhe
     {
         static void Main(string[] args)
         {
+            //esercizio n1 pag 262
             Console.Write("Inserisci la stringa:  ");
             string s1 = Console.ReadLine();
             Console.Write("Inserisci la stringa:  ");
             string s2 = Console.ReadLine();
             bool risposta = MetodiStringhe.AreEquals(s1, s2);
             Console.WriteLine($"la risposta è {risposta}");
+
+            //esercizio n2 pag 262
             Console.Write("Inserisci la stringa:  ");
             string s3 = Console.ReadLine();
             bool risposta1 = MetodiStringhe.ContainsDoubleChar(s3);
             Console.WriteLine($"la risposta è {risposta1}");
+
+            //esercizio n11 pag 262
             Console.Write("Inserisci la stringa:  ");
             string s4 = Console.ReadLine();
             string risposta2 = MetodiStringhe.Convert(s4);
